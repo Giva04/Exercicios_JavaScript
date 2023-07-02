3fe616bf2b6a63e39d0aa3faaf23f44bc6608357
Giva Gomes <givagomes04@gmail.com>
2023-07-02 14:28:17 -0300

    verificandor de numeros

---
+++ exerc_05/index.js
@@ -0,0 +1,16 @@
+//VERIFICANDO NUMERO NEGATIVO 
+
+let numero = parseFloat(prompt("Digite um número: "))
+
+    if (numero > 0) {
+        console.log("O numero é positivo")
+
+    }else if(numero < 0) {
+        console.log("O numero é negativo")
+
+    } else {
+        console.log("O numero é Zero")
+    }
+
+
+
